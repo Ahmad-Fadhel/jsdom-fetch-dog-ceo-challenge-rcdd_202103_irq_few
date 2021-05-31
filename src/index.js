e8c35afc13db5c1317be7cc5894f0e940948f724
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-31 14:43:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetched(){
+  
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(recevied){
+    return recevied.json();
+  })
+  .then(function(json){
+    
+  })
+}
+
+window.addEventListener("load",fetched())
